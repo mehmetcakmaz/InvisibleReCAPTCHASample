da95207e0a01c272251478439f0c50d9ae42416e
Mehmet-Guest <Mehmet-Guest@MEHMETS>
2017-07-23 22:11:07 +0300

    Add project files.

---
+++ InvisibleReCAPTCHA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InvisibleReCAPTCHA.Startup))]
+namespace InvisibleReCAPTCHA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
